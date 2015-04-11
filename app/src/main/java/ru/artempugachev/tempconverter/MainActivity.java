e5d097eff44ea29ba9d110788c8220fbae8e6770
artem-p <artempugachev1@gmail.com>
2015-04-11 13:07:46 +0300

    Заготовка обработки кнопок перевода

--- app/src/main/java/ru/artempugachev/tempconverter/MainActivity.java
+++ app/src/main/java/ru/artempugachev/tempconverter/MainActivity.java
@@ -1,24 +1,42 @@
 package ru.artempugachev.tempconverter;
 
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
     private TextView m_tvCelsius;
     private TextView m_tvFahr;
+    private ImageButton m_btnCtoF;
+    private ImageButton m_btnFtoC;
+    private Temperature m_temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        m_tvCelsius = (TextView) findViewById(R.id.tvCelsius);
-        m_tvFahr = (TextView) findViewById(R.id.tvFahr);
+        getViews();
     }
 
+    private void getViews() {
+        //  Получаем все элементы управления
+        m_tvCelsius = (TextView) findViewById(R.id.tvCelsius);
+        m_tvFahr = (TextView) findViewById(R.id.tvFahr);
+        m_btnCtoF = (ImageButton) findViewById(R.id.btnCtoF);
+        m_btnFtoC = (ImageButton) findViewById(R.id.btnFtoC);
+
+        m_btnCtoF.setOnClickListener(ctoFListener);
+        m_btnFtoC.setOnClickListener(fToCListener);
+
+        m_temperature = new Temperature();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,4 +65,20 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private View.OnClickListener ctoFListener = new View.OnClickListener(){
+
+        public void onClick(View v) {
+            m_tvFahr.setText("ctof");
+        }
+    };
+
+    private View.OnClickListener fToCListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            m_tvCelsius.setText("ftoc");
+        }
+    };
 }
+
