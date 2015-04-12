174cc0f8f8cc1a2ec765023f2604591aca5d77f5
artem-p <artempugachev1@gmail.com>
2015-04-12 11:41:40 +0300

    Убираем пока сеттингс

--- app/src/main/java/ru/artempugachev/tempconverter/MainActivity.java
+++ app/src/main/java/ru/artempugachev/tempconverter/MainActivity.java
@@ -53,9 +53,9 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         if(id == R.id.action_refresh) {
             //  Очищаем поля ввода
